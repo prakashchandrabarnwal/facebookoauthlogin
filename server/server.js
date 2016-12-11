680efef21f3b1f73fdcc9340b244e3d17a0ead5c
prakash.chandra.barnwal <barnwalbikky@gmail.com>
2016-12-12 01:04:17 +0530

    heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var express = require('express');
 var path = require("path");
-var PORT = 8080 || process.env.PORT;
+var PORT = process.env.PORT || 8080 ;
 
 
 app.use(express.static(__dirname + './../client/'));
